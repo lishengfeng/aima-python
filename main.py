78916b25ba15db2da73bbdf56e2ef678a367fc8b
Shengfeng Li <cs.shengfeng.li@gmail.com>
2018-10-27 22:00:33 -0500

    Pull lib to local

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import search
+import functools
+
+import heapq
 
 def h(node):
     state = node.state
@@ -13,13 +15,320 @@ def h(node):
             mhd = abs(dirty_x - x) + abs(dirty_y - y) + mhd
     return sum_dirty + mhd
 
+class Problem(object):
 
-vaccum_problem = search.VacuumClean((1,1,1,0,0,0,0,0,0,4))
+    """The abstract class for a formal problem. You should subclass
+    this and implement the methods actions and result, and possibly
+    __init__, goal_test, and path_cost. Then you will create instances
+    of your subclass and solve them with the various search functions."""
 
-node_h, num_node_gen_h = search.astar_search(vaccum_problem, h)
+    def __init__(self, initial, goal=None):
+        """The constructor specifies the initial state, and possibly a goal
+        state, if there is a unique goal. Your subclass's constructor can add
+        other arguments."""
+        self.initial = initial
+        self.goal = goal
+
+    def actions(self, state):
+        """Return the actions that can be executed in the given
+        state. The result would typically be a list, but if there are
+        many actions, consider yielding them one at a time in an
+        iterator, rather than building them all at once."""
+        raise NotImplementedError
+
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
+        raise NotImplementedError
+
+    def goal_test(self, state):
+        """Return True if the state is a goal. The default method compares the
+        state to self.goal or checks for state in self.goal if it is a
+        list, as specified in the constructor. Override this method if
+        checking against a single self.goal is not enough."""
+        if isinstance(self.goal, list):
+            return is_in(state, self.goal)
+        else:
+            return state == self.goal
+
+    def path_cost(self, c, state1, action, state2):
+        """Return the cost of a solution path that arrives at state2 from
+        state1 via action, assuming cost c to get up to state1. If the problem
+        is such that the path doesn't matter, this function will only look at
+        state2.  If the path does matter, it will consider c and maybe state1
+        and action. The default method costs 1 for every step in the path."""
+        return c + 1
+
+    def value(self, state):
+        """For optimization problems, each state has a value.  Hill-climbing
+        and related algorithms try to maximize this value."""
+        raise NotImplementedError
+# ______________________________________________________________________________
+
+class VacuumClean(Problem):
+    """ The problem of sliding tiles numbered with 0 or 1 on a 3x3 board plus the current location of agent,
+    where the last element is the location of the agent. A state is represented as a 3x3 list,
+    where element at index i,j represents the status of clean/dirty (0 if it's clean)
+    example:
+        Initial State                         Goal State
+        | 1 | 1 | 1 |                        | 0 | 0 | 0 |
+        | 0 | 0 | 0 |                        | 0 | 0 | 0 |
+        | 0 | 0 | 0 |                        | 0 | 0 | 0 |
+        with 4 agent location(2,2)            regardless of the location of agent
+    """
+
+
+    def __init__(self, initial, goal=(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)):
+        """ Define goal state and initialize a problem """
+
+        self.goal = goal
+        Problem.__init__(self, initial, goal)
+
+    def actions(self, state):
+        """ Return the actions that can be executed in the given state.
+        The result would be a list, since there are only five possible actions
+        in any given state of the environment """
+
+        possible_actions = ['SUCK', 'UP', 'LEFT', 'DOWN', 'RIGHT']
+        loc_agent = state[-1]
+
+        if loc_agent % 3 == 0:
+            possible_actions.remove('LEFT')
+        if loc_agent < 3:
+            possible_actions.remove('UP')
+        if loc_agent % 3 == 2:
+            possible_actions.remove('RIGHT')
+        if loc_agent > 5:
+            possible_actions.remove('DOWN')
+
+        return possible_actions
+
+    def result(self, state, action):
+        """ Given state and action, return a new state that is the result of the action.
+        Action is assumed to be a valid action in the state """
+
+        # current agent's location
+        loc_agent = state[-1]
+
+        new_state = list(state)
+
+        if 'SUCK'.upper() == action.upper():
+            new_state[loc_agent] = 0
+        else:
+            delta = {'UP':-3, 'DOWN':3, 'LEFT':-1, 'RIGHT':1}
+            new_state[-1] = loc_agent + delta[action]
+
+        return tuple(new_state)
+
+    def goal_test(self, state):
+        """ Given a state, return True if state is a goal state(Ignore agent location) or False, otherwise """
+
+        return state[:-1] == self.goal[:-1]
+
+    def value(self, state):
+        pass
+
+    def path_cost(self, c, state1, action, state2):
+        """Return the cost of a solution path that arrives at state2 from
+        state1 via action, assuming cost c to get up to state1. If the problem
+        is such that the path doesn't matter, this function will only look at
+        state2.  If the path does matter, it will consider c and maybe state1
+        and action. The default method costs 1 for every step in the path."""
+        return c + 1 + 2 * state2[:-1].count(1)
+
+    def h(self, node):
+        """ Return the heuristic value for a given state.
+        Default heuristic function used is
+        h(n) = number of dirty squares """
+
+        return sum(s != g for (s, g) in zip(node.state[:-1], self.goal[:-1]))
+
+class Node:
+
+    """A node in a search tree. Contains a pointer to the parent (the node
+    that this is a successor of) and to the actual state for this node. Note
+    that if a state is arrived at by two paths, then there are two nodes with
+    the same state.  Also includes the action that got us to this state, and
+    the total path_cost (also known as g) to reach the node.  Other functions
+    may add an f and h value; see best_first_graph_search and astar_search for
+    an explanation of how the f and h values are handled. You will not need to
+    subclass this class."""
+
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        """Create a search tree Node, derived from a parent by an action."""
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
+
+    def __repr__(self):
+        return "<Node {}>".format(self.state)
+
+    def __lt__(self, node):
+        return self.state < node.state
+
+    def expand(self, problem):
+        """List the nodes reachable in one step from this node."""
+        return [self.child_node(problem, action)
+                for action in problem.actions(self.state)]
+
+    def child_node(self, problem, action):
+        """[Figure 3.10]"""
+        next_state = problem.result(self.state, action)
+        next_node = Node(next_state, self, action,
+                         problem.path_cost(self.path_cost, self.state,
+                                           action, next_state))
+        return next_node
+
+    def solution(self):
+        """Return the sequence of actions to go from the root to this node."""
+        return [{'Action': node.action, 'State':node.state, 'Path_Cost': node.path_cost} for node in self.path()[1:]]
+
+    def path(self):
+        """Return a list of nodes forming the path from the root to this node."""
+        node, path_back = self, []
+        while node:
+            path_back.append(node)
+            node = node.parent
+        return list(reversed(path_back))
+
+    # We want for a queue of nodes in breadth_first_graph_search or
+    # astar_search to have no duplicated states, so we treat nodes
+    # with the same state as equal. [Problem: this may not be what you
+    # want in other contexts.]
+
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.state == other.state
+
+    def __hash__(self):
+        return hash(self.state)
+
+
+def best_first_graph_search(problem, f):
+    """Search the nodes with the lowest f scores first.
+    You specify the function f(node) that you want to minimize; for example,
+    if f is a heuristic estimate to the goal, then we have greedy best
+    first search; if f is node.depth then we have breadth-first search.
+    There is a subtlety: the line "f = memoize(f, 'f')" means that the f
+    values will be cached on the nodes as they are computed. So after doing
+    a best first search you can examine the f values of the path returned."""
+    f = memoize(f, 'f')
+    node = Node(problem.initial)
+    frontier = PriorityQueue('min', f)
+    frontier.append(node)
+    explored = set()
+    num_node_gen = 0
+    while frontier:
+        node = frontier.pop()
+        if problem.goal_test(node.state):
+            return node, num_node_gen
+        explored.add(node.state)
+        for child in node.expand(problem):
+            num_node_gen = num_node_gen + 1
+            if child.state not in explored and child not in frontier:
+                frontier.append(child)
+            elif child in frontier:
+                incumbent = frontier[child]
+                if f(child) < f(incumbent):
+                    del frontier[incumbent]
+                    frontier.append(child)
+    return None
+
+def astar_search(problem, h=None):
+    """A* search is best-first graph search with f(n) = g(n)+h(n).
+    You need to specify the h function when you call astar_search, or
+    else in your Problem subclass."""
+    h = memoize(h or problem.h, 'h')
+    return best_first_graph_search(problem, lambda n: n.path_cost + h(n))
+
+
+def memoize(fn, slot=None, maxsize=32):
+    """Memoize fn: make it remember the computed value for any argument list.
+    If slot is specified, store result in that slot of first argument.
+    If slot is false, use lru_cache for caching the values."""
+    if slot:
+        def memoized_fn(obj, *args):
+            if hasattr(obj, slot):
+                return getattr(obj, slot)
+            else:
+                val = fn(obj, *args)
+                setattr(obj, slot, val)
+                return val
+    else:
+        @functools.lru_cache(maxsize=maxsize)
+        def memoized_fn(*args):
+            return fn(*args)
+
+    return memoized_fn
+
+def is_in(elt, seq):
+    """Similar to (elt in seq), but compares with 'is', not '=='."""
+    return any(x is elt for x in seq)
+
+class PriorityQueue:
+    """A Queue in which the minimum (or maximum) element (as determined by f and
+    order) is returned first.
+    If order is 'min', the item with minimum f(x) is
+    returned first; if order is 'max', then it is the item with maximum f(x).
+    Also supports dict-like lookup."""
+
+    def __init__(self, order='min', f=lambda x: x):
+        self.heap = []
+
+        if order == 'min':
+            self.f = f
+        elif order == 'max':  # now item with max f(x)
+            self.f = lambda x: -f(x)  # will be popped first
+        else:
+            raise ValueError("order must be either 'min' or max'.")
+
+    def append(self, item):
+        """Insert item at its correct position."""
+        heapq.heappush(self.heap, (self.f(item), item))
+
+    def extend(self, items):
+        """Insert each item in items at its correct position."""
+        for item in items:
+            self.heap.append(item)
+
+    def pop(self):
+        """Pop and return the item (with min or max f(x) value
+        depending on the order."""
+        if self.heap:
+            return heapq.heappop(self.heap)[1]
+        else:
+            raise Exception('Trying to pop from empty PriorityQueue.')
+
+    def __len__(self):
+        """Return current capacity of PriorityQueue."""
+        return len(self.heap)
+
+    def __contains__(self, item):
+        """Return True if item in PriorityQueue."""
+        return (self.f(item), item) in self.heap
+
+    def __getitem__(self, key):
+        for _, item in self.heap:
+            if item == key:
+                return item
+
+    def __delitem__(self, key):
+        """Delete the first occurrence of key."""
+        self.heap.remove((self.f(key), key))
+        heapq.heapify(self.heap)
+
+
+
+vaccum_problem = VacuumClean((1,1,1,0,0,0,0,0,0,4))
+
+node_h, num_node_gen_h = astar_search(vaccum_problem, h)
 print(node_h.solution())
 print("Number of nodes generated using h: {}".format(num_node_gen_h))
 
-node_h2, num_node_gen_h2 = search.astar_search(vaccum_problem)
+node_h2, num_node_gen_h2 = astar_search(vaccum_problem)
 print(node_h2.solution())
 print("Number of nodes generated using h2: {}".format(num_node_gen_h2))
